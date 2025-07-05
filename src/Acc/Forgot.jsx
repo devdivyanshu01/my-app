0d9bb2bad0099c4e66c3bd62e6ded01d8c7023c2
Divyanshu Dhiman <divyanshudhiman51@example.com>
2025-07-06 01:10:00 +0530

    my-final

--- src/Acc/Forgot.jsx
+++ src/Acc/Forgot.jsx
@@ -10,7 +10,7 @@ function Forgot(){
         <div className="sign">
             <div className="container" style={{height:"400px"}}>
                 <p style={{color: 'black', maxWidth: "380px", margin: '0 auto', textAlign: 'center'}}>Don’t worry, Enter your email below and we will send you a link to change password.</p>
-                <div className="upper"><input className='mail' type="mail" placeholder="Email" pattern="[a-zA-Z0-9@.]" required style={{marginTop:"50px", marginBottom:"40px"}}/>
+                <div className="upper"><input className='email' type="mail" placeholder="Email" pattern="[a-zA-Z0-9@.]" required style={{marginTop:"50px", marginBottom:"40px"}}/>
                 <button className="btn" style={{backgroundColor:"rgb(95, 255, 47)", marginBottom:"20px"}}>Submit</button>
                 <button className="btn" style={{backgroundColor:"rgb(35, 87, 200)"}}>Sign UP</button></div>
 
